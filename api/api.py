0328d3a062cda3c80ab497c3a19083e570f833ce
afansky <afansky@gmail.com>
2021-04-10 15:41:52 -0700

    fixing build

--- api/api.py
+++ api/api.py
@@ -1,13 +1,35 @@
 from flask import Flask
 from flask import request
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="mysql",
+    user="root",
+    password="root",
+    database="hummingbot"
+)
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def store():
+    json = request.get_json()
+
     print("got a request")
-    return "YO"
+
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO bots (name, trades) VALUES (%s, %s)"
+    val = (json.name, json.trades)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+    return """
+    {
+        success: true
+    }
+    """
 
 
 if __name__ == '__main__':
